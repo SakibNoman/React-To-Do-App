397fcbe6162ecbae4516ace9519cd7f8211b6930
Sakib Noman <skbnmn@gmail.com>
2021-03-11 22:40:10 +0600

    structure build

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import ToDoList from '../ToDoList/ToDoList';
+
+const Home = () => {
+
+    const [list, setList] = useState([]);
+
+    let inputValue = '';
+    const takeInput = (e) => {
+        inputValue = e.target.value;
+    }
+
+    let newItem = '';
+    const addNewItem = () => {
+        if (inputValue.length > 0) {
+            newItem = [inputValue, ...list]
+            setList(newItem);
+        }
+    }
+
+    return (
+        <div>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                </div>
+                <input onChange={takeInput} type="text" placeholder="Create a new Todo" className="form-control" aria-label="Text input with checkbox" />
+                <div className="input-group-prepend">
+                    <button onClick={addNewItem} className="btn btn-outline-secondary" type="button" id="button-addon1">Add</button>
+                </div>
+            </div>
+            {
+                list.map(each => <ToDoList note={each} ></ToDoList>)
+            }
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/ToDoList/ToDoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ToDoList = (props) => {
+    return (
+        <div>
+            <div className="input-group-text mb-2  justify-content-between ">
+                <input className="mr-3" type="checkbox" aria-label="Checkbox for following text input" />
+                <div class="input-group-prepend text-left">
+                    {props.note}
+                </div>
+                <div className="text-center flex-grow-1" ></div>
+                <div class="input-group-prepend">
+                    <button class="btn btn-outline-secondary ml-2" type="button" id="button-addon1">X</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ToDoList;
